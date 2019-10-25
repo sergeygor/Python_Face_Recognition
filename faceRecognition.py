4b40c4b412d498c6db8223cabd7f3b04e454a842
sergeygor <sgordeev68git@gmail.com>
2019-10-24 22:14:33 -0700

    DISPLAY HOW MANY PEOPLE ON THE IMAGE

--- faceRecognition.py
+++ faceRecognition.py
@@ -6,3 +6,5 @@ image = face_recognition.load_image_file('unknownFace/group01.jpg')
 face_location = face_recognition.face_locations(image)
 # Array of coordinates of each face
 print(face_location)
+# Find number of people in the image
+print(f'There are {len(face_location)} people in this image.')
