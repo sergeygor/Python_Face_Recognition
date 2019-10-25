2cb729e1276340ece6bd1382f529e23fa39ef941
sergeygor <sgordeev68git@gmail.com>
2019-10-24 22:08:35 -0700

    EXPLORE FACES LOCATIONS

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,1 +1,8 @@
+import face_recognition
 
+# Get image
+image = face_recognition.load_image_file('unknownFace/group01.jpg')
+# Get faces
+face_location = face_recognition.face_locations(image)
+# Array of coordinates of each face
+print(face_location)
